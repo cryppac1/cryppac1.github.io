ac41396d7812c74c324d778f382755b550f29734
cryppac1 <112296580+cryppac1@users.noreply.github.com>
2022-08-31 14:19:13 +0530

    mobile version

---
+++ button.js
@@ -0,0 +1,5 @@
+let button = document.querySelector(".arrow"),
+links = document.querySelector(".navi");
+   button.addEventListener("click",()=>{
+          links.classList.toggle("display");
+   })
